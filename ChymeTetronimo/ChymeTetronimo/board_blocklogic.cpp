c8994790c406b38c114df1f5427060e76c6f21bd
Kyle Chynoweth <kchymet@gmail.com>
2014-08-31 01:16:41 -0400

    Added enum for movement directions and cpp files to implement headers

---
+++ ChymeTetronimo/ChymeTetronimo/board_blocklogic.cpp
@@ -0,0 +1,8 @@
+#include "board.h"
+
+namespace Tetronimo{
+	void Board::placeFallingBlock(){}
+	bool Board::checkCollision(){}
+	bool Board::checkContact(){}
+	bool Board::checkValid(){}
+}
---
+++ ChymeTetronimo/ChymeTetronimo/board_blockmotion.cpp
@@ -0,0 +1,15 @@
+#include "board.h"
+
+namespace Tetronimo{
+	void Board::moveFallingBlock(move_direction_t){
+
+	}
+	void Board::rotateFallingBlock(rotate_direction_t){
+
+	}
+	void Board::softDropFallingBlock(){
+	}
+	void Board::hardDropFallingBlock(){
+
+	}
+}
